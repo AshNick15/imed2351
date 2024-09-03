3ac44e4d5e3dbad1375422f75b8023d4e2c8066f
AshNick15 <salinasashley164@gmail.com>
2024-09-02 20:14:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello, what time are you seeing this?') ;
+var life = 100; // Life Bar GLOBAL VARIABLE
+
+life = life - 10; 
+
+//90
+
+let name = 'Ashley Salinas'; // name display BLOCK VARIABLE
+
+let title = 'Hello, from Ashley Salinas'; // STRING
+
+const person = { occupation: 'Software Developer'} // OBJECT
+
+console.log(title);
