77d66edc2f3fc09e366d8c37a79e50bac4513163
AshNick15 <salinasashley164@gmail.com>
2024-09-12 22:50:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,31 @@ function toUpper(text) {
 
 toUpper(name);
 
+const person = {
+        firstName: "Ashley",
+        lastName : "Salinas",
+        age      :  29
+};
+
+function info() {
+    let x = "firstName";
+    let y = "age";
+    document.getElementById("info").innerHTML = person[x] + " is " + person[y] + " years old.";
+}
+
+info();
+
+function displayDate() {
+    document.getElementById('showdate').innerHTML = Date();
+}
+
+displayDate();
+
 function logger()  {
     console.log('I hope I am doing this right!');
 }
 
 logger();
-
 
 function adder(num1, num2, num3) {
     console.log (num1 + num2 - num3)
