6aaeed916b91648541de0a1afc32150813f91c9b
AshNick15 <salinasashley164@gmail.com>
2024-09-26 13:18:13 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,69 +1,27 @@
-const first = "Ashley";         //this is my concatenated string variable.
-const town = "Del Rio, Tx"
 
-console.log(`Hello I'm your classate and my name is ${first} and I am from ${town}.`);
+const button = document.getElementById("myButton");
+const paragraph = document.getElementById("myParagraph");
 
-//end
+button.addEventListener("click", () => {
+    paragraph.classList.toggle("hidden");
+});
 
-const person = {
-        firstName: "Ashley",
-        lastName : "Salinas",
-        age      :  29
-};
-
-function info() {
-    let x = "firstName";
-    let y = "age";
-    document.getElementById("info").innerHTML = person[x] + " is " + person[y] + " years old.";
+for (let i = 18;i < 26; i++) {      //this is my forLoop
+    console.log(i); 
 }
+    //end
 
-info();
+const colors = [`Orange`, `Black`, `White`] ;      //this is my whileLoop
 
-function displayDate() {
-    document.getElementById('showdate').innerHTML = Date();
+let color = 0;
+
+while (color < colors.length) {
+    console.log(`${colors[color]}`);
+    color++;
 }
-
-displayDate();
+  //end
 
 function logger()  {
     console.log('I hope I am doing this right!');
 }
-
 logger();
-
-const old = 19;     //this is my "if" statement.
-
-if(old > 21) {
-    console.log("You are free to enter.");
-} else if (old < 21) {
-    console.log("Sorry, you are not able to enter.");
-}
-//end
-
-let fruit = 'peaches';      //this is my switch statement.
-
-switch (fruit) {
-    case 'peaches':
-        console.log('Peaches are my favorite.');
-        break;
-    case 'apple':
-        console.log('Apples are good too.');
-        break;
-    case 'mango':
-        console.log('Mangoes are even better than Apples.');
-        break;
-}
-//end
-
-let course = "    JavaScript makes me overthink.     ";     //this is my string method.
-    console.log(course.trim());
-
-//end
-
-const PI = 3.141596254      //this is my number method.
-
-const num = PI.toFixed(2);
-
-console.log(num);
-
-//end
